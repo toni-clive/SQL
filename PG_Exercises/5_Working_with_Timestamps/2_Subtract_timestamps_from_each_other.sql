ea96a0eec068b884f57963ef0dc9a4bc2f48dd0c
toni-clive <clivetoniato@googlemail.com>
2026-09-10 09:01:47 +0100

    started Working with Timestamps section

--- PG_Exercises/5_Working_with_Timestamps/2_Subtract_timestamps_from_each_other.sql
+++ PG_Exercises/5_Working_with_Timestamps/2_Subtract_timestamps_from_each_other.sql
@@ -1,1 +1,7 @@
 -- https://pgexercises.com/questions/date/interval.html
+
+SELECT * FROM generate_series(
+    '2012-10-01'::timestamp, 
+    '2012-10-31'::timestamp, 
+    '1 day'::interval 
+) ts;
--- PG_Exercises/5_Working_with_Timestamps/3_Generate_a_list_of_all_the_dates_in_October_2012.sql
+++ PG_Exercises/5_Working_with_Timestamps/3_Generate_a_list_of_all_the_dates_in_October_2012.sql
@@ -1,1 +1,3 @@
 -- https://pgexercises.com/questions/date/series.html
+
+SELECT EXTRACT(day FROM '2012-08-31'::timestamp) date_part;
